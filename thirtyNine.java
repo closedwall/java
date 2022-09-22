52eacd8a802b704ea5371c7d8b91c06f067451af
Rajesh Yadav <yadavrajesh5612@gmail.com>
2022-09-22 12:58:57 +0530

    second commit

--- thirtyNine.java
+++ thirtyNine.java
@@ -1,7 +1,13 @@
 import java.util.Scanner;
-
+// programme to split the string in java
 public class thirtyNine {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String [] arrstr = str.split("a",5);
+        for(String a: arrstr)
+        {
+            System.out.println(a);
+        }
     }
 }
