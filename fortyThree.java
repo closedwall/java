816bd1818dcbb538baecf89d504de045cd097494
Rajesh Yadav <yadavrajesh5612@gmail.com>
2022-09-24 11:44:59 +0530

    add content

--- fortyThree.java
+++ fortyThree.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class fortyThree {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String str2 = sc.nextLine();
+        sc.close();
     }
 }
