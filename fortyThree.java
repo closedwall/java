bc52a19d122d13adc6a27f3827aa7b22d71118b2
Rajesh Yadav <yadavrajesh5612@gmail.com>
2022-09-24 12:00:12 +0530

    modified content

--- fortyThree.java
+++ fortyThree.java
@@ -5,6 +5,10 @@ public class fortyThree {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         String str2 = sc.nextLine();
+        System.out.println("the length of "+str+" is:"+str.length());
+        System.out.println("the length of "+str2+" is:"+str2.length());
+        String concat = str+" "+str2;
+        System.out.println("The concatenated string is: "+concat);
         sc.close();
     }
 }
